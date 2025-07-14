32312073b6144b301625f0449033dc58d5d0ecf4
Stanislav Pyskir <stasikpyskir@example.com>
2025-07-14 14:14:54 +0300

    login and logout

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -6,22 +6,24 @@ import Main from './components/Main'
 import Register from './components/Register'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Login from './components/Login'
-
+import AuthProvider from './AuthProvider'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <BrowserRouter>
-    <Header />
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login  />} />
-      </Routes>
-    <Footer />
-    </ BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+      <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login  />} />
+        </Routes>
+      <Footer />
+      </ BrowserRouter>
+    </AuthProvider>
     </>
   )
 }
